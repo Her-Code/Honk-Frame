6b43061617644c382c9752c13516605918d5f7b5
+Sharon <+wanjiku.kahira@gmail.com>
2025-07-06 19:50:45 +0300

    Modify serveless api

--- api/honk-response.ts
+++ api/honk-response.ts
@@ -1,18 +1,31 @@
 // /api/honk-response.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req: any, res: any) {
   res.status(200).json({
     frame: {
       version: 'vNext',
       image: 'https://honk-frame.vercel.app/frame-preview.png',
       button: [
-        {
-          label: '🪿 Another Honk'
-        }
+        { label: '🪿 Another Honk' }
       ],
       post_url: 'https://honk-frame.vercel.app/api/honk-response'
     }
   });
 }
+
+// import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+// export default function handler(req: VercelRequest, res: VercelResponse) {
+//   res.status(200).json({
+//     frame: {
+//       version: 'vNext',
+//       image: 'https://honk-frame.vercel.app/frame-preview.png',
+//       button: [
+//         {
+//           label: '🪿 Another Honk'
+//         }
+//       ],
+//       post_url: 'https://honk-frame.vercel.app/api/honk-response'
+//     }
+//   });
+// }
 
